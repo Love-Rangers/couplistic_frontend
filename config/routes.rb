22a64ae18a06656168b6b71d552ea9cd51fda10f
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-08 16:45:38 -0600

    Add ticketmaster view with event results

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :sessions, only: :new
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   get '/weather', to: 'weather#show'
+  get '/ticketmaster', to: 'ticketmaster#index'
+
   #catch all for rogue uri's
   get '*pages', to: 'welcome#index'
 end
