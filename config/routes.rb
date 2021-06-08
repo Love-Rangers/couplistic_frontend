43989768fe80eb7291900bf4e630d7d03f340b6d
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-08 14:13:48 -0600

    Add endpoint consumption for weather

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resource :spotify, only: :new, controller: 'spotify'
   resources :sessions, only: :new
   delete '/logout', to: 'sessions#destroy', as: 'logout'
-
+  get '/weather', to: 'weather#show'
   #catch all for rogue uri's
   get '*pages', to: 'welcome#index'
 end
