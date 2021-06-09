183168d4ba2938e445ce580df983746029eb503d
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-09 17:11:02 -0600

    Modify redirect uri

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -3,8 +3,8 @@ class SpotifyController < ApplicationController
     scope = 'user-read-recently-played user-read-email'
     redirect_uri = 'https://couplistic-fe.herokuapp.com/sessions/new' # needs to be heroku address
     #First step of OAuth => to authorization server with Client id and redirect_uri
-    redirect_to 'https://accounts.spotify.com/authorize' +
-    "?response_type=code" + "&client_id=#{ENV['client_id']}" +
+    redirect_to 'https://accounts.spotify.com/authorize?' +
+    "response_type=code" + "&client_id=#{ENV['client_id']}" +
     "&scope=#{scope}" + "&redirect_uri=#{redirect_uri}"
     #Will redirect_to a new session (localhost:3000//sessions/new)
   end
