36ec289f90c4cb27657d111217351b13fb7a4fb8
Brisa Garcia <brisagarciaglz@gmail.com>
2021-06-09 16:43:58 -0600

    Remove unused supporting files

--- app/controllers/ticketmaster_controller.rb
+++ app/controllers/ticketmaster_controller.rb
@@ -4,6 +4,7 @@ class TicketmasterController < ApplicationController
     keyword = params[:keyword]
 
     response = Faraday.get("https://couplistic-be.herokuapp.com/api/v1/ticketmaster?city=#{city}&keyword=#{keyword}")
+    # binding.pry
     json = JSON.parse(response.body, symbolize_names: true)
 
     @tix_events = json[:data]
