8923bb3becc243c733911badfac16e8bb1725bd5
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-08 22:50:17 -0600

    Add carrot stuff

--- app/facades/spotify_user_facade.rb
+++ app/facades/spotify_user_facade.rb
@@ -3,9 +3,11 @@ class SpotifyUserFacade
     response = Faraday.get('https://api.spotify.com/v1/me', nil, { "Authorization" => "Bearer #{access_token}" } )
     user_spotify_info = JSON.parse(response.body, symbolize_names: true)
     #set the user and render dashboard/index
+binding.remote_pry
     @user = User.where(
       display_name: user_spotify_info[:display_name],
       email: user_spotify_info[:email]
     ).first_or_create
+binding.remote_pry
   end
 end
