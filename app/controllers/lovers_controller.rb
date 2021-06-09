e1f207a07a7a1ad13f2f3c0345fc2c75c7d3a7e9
r-stemmle <ryan.stemmle@gmail.com>
2021-06-08 22:11:02 -0400

    Add event index and post functionality for creating event

--- app/controllers/lovers_controller.rb
+++ app/controllers/lovers_controller.rb
@@ -10,7 +10,7 @@ class LoversController < ApplicationController
       # @events = CouplisticFacade.get_events(@user.id)
       @next_full_moon = Time.at(FullMoonFacade.next_full_moon).strftime("%A, %b %e")
       @songs_n_artists = SpotifyGemFacade.get_recommendations
-
+      
       render 'dashboard/index'
     end
   end
