347e44cba378389827dd30fed6a713225d83837d
Kseniya K <kseniya.kudelich@gmail.com>
2021-06-10 15:02:17 -0600

    Add sad path for weather

--- spec/features/lovers/index_spec.rb
+++ spec/features/lovers/index_spec.rb
@@ -39,8 +39,11 @@ describe 'As an authenticated user when I visit the  dashboard' do
 
       expect(page).to have_content("Current Weather")
       expect(page).to have_field(:q)
-      fill_in :q, with: "Denver"
 
+      click_button("Is It Sunny?")
+      expect(page).to have_content("Sorry, currently no weather 😞")
+
+      fill_in :q, with: "Denver"
       click_button("Is It Sunny?")
       expect(current_path).to eq('/weather')
     end
