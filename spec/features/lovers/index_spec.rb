1fe2f4161b97048de9444c2a04c89697b9e16af8
austinandrade <austinmandrade@gmail.com>
2021-06-10 16:33:02 -0400

    Attempt git merge main into working branch

--- spec/features/lovers/index_spec.rb
+++ spec/features/lovers/index_spec.rb
@@ -16,34 +16,32 @@ describe 'As an authenticated user when I visit the  dashboard' do
     it "shows a search form for events by the keyboard or city" do
       visit lovers_path
 
-      expect(page).to have_content("Tickets?")
+      expect(page).to have_content("Search Event Tickets")
       expect(page).to have_field(:city)
       expect(page).to have_field(:keyword)
-      expect(page).to have_button("Find")
+      expect(page).to have_button("Search")
     end
 
     it "when i fill out the form, i am redirected to TM path", :vcr do
       visit lovers_path
 
-      within ".tm-search" do
-        fill_in :city, with: "Morrsion"
-        fill_in :keyword, with: "Reggae"
-        click_button("Find")
-      end
+      fill_in :city, with: "Morrison"
+      fill_in :keyword, with: "Reggae"
+      click_button("Search")
+
       expect(current_path).to eq(ticketmaster_path)
     end
   end
 
   describe 'Weather Form' do
-    it "can search for weather by location", :vcr do
+    it "can search for weather by location" do
       visit lovers_path
 
-      expect(page).to have_content("Weather?")
+      expect(page).to have_content("Current Weather")
       expect(page).to have_field(:q)
+      fill_in :q, with: "Denver"
 
-      within '.weather' do
-        click_button("Find")
-      end
+      click_button("Is It Sunny?")
       expect(current_path).to eq('/weather')
     end
   end
